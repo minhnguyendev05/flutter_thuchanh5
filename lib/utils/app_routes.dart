fa66b40e1b5c0effe31ace5580ed41d8d920439a
Thuhuong140305 <huongdang2k5@gmail.com>
2026-03-19 22:08:21 +0700

    UI FORM + CRUD + DETAIL SCREEN

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -3,5 +3,6 @@ class AppRoutes {
   static const String login = '/login';
   static const String home = '/home';
   static const String transactionForm = '/transaction-form';
+  static const String transactionDetail = '/transaction-detail';
   static const String statistics = '/statistics';
 }
